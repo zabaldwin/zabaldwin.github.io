f8df9c1a1e4767848271b773254dca31c0ae3ea8
Zach Baldwin <zbaldwin@andrew.cmu.edu>
2021-08-15 01:40:44 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -13,5 +13,35 @@ var addressPoints = [
     "Glasgow, Scotland",
     55.86515,
     -4.25763
+  ],
+  [
+    "Williamsburg, Virginia",
+    37.2705556,
+    -76.7077778
+  ],
+  [
+    "Newport News, Virginia",
+    37.0870821,
+    -76.4730122
+  ],
+  [
+    "Columbus, Ohio",
+    39.983334,
+    -82.983330
+  ],
+  [
+    "Cambridge, Massachusetts",
+    42.373611,
+    -71.110558
+  ],
+  [
+    "Edinburgh, Scotland",
+    55.95206,
+    -3.19648
+  ],
+  [
+    "Mainz, Germany",
+    49.98419,
+    -8.2791
   ]
 ];
