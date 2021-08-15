d84b68c10f4bf2fae6ec32c764db616f79fd85fd
Zach Baldwin <zbaldwin@andrew.cmu.edu>
2021-08-15 00:23:45 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -3,5 +3,15 @@ var addressPoints = [
     "Waikoloa, Hawaii",
     19.936533,
     -155.789495
+  ], 
+  [
+    "Pittsburgh, Pennsylvania",
+    40.440763,
+    -79.99589
+  ],
+  [
+    "Glasgow, Scotland",
+    55.86515,
+    -4.25763
   ]
 ];
