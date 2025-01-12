8de5b7e8bc35c12ac3a873ac427035428da48b9d
Zachary Baldwin <zbaldwin@andrew.cmu.edu>
2025-01-12 01:11:24 -0500

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -10,12 +10,12 @@ var addressPoints = [
     -79.99589
   ],
   [
-    "Glasgow (University of Glasgow)",
+    "Glasgow, Scotland (University of Glasgow)",
     55.86515,
     -4.25763
   ],
   [
-    "Williamsburg (College of William & Mary)",
+    "Williamsburg (The College of William & Mary)",
     37.2705556,
     -76.7077778
   ],
@@ -25,22 +25,57 @@ var addressPoints = [
     -76.4730122
   ],
   [
+    "Norfolk, Virginia",
+    36.850769,
+    -76.285873
+  ]
+  [
     "Columbus, Ohio",
     39.983334,
     -82.983330
   ],
   [
-    "Cambridge, Massachusetts",
-    42.373611,
-    -71.110558
+    "Madison, Wisconsin (University of Wisconsin)",
+    43.073051,
+    -89.401230
   ],
   [
-    "Edinburgh, Scotland",
-    55.95206,
-    -3.19648
+    "West Lafayette, Indiana (Purdue University)",
+    40.423705,
+    -86.921195
   ],
   [
-    "Mainz (Mainzer Mikrotron)",
+    "Princeton, New Jersey (Princeton University)",
+    40.343990,
+    -74.651448
+  ],
+  [
+    "Athens, Greece",
+    37.983810,
+    23.727539
+  ],
+  [
+    "Barcelona, Spain (Universitat de Barcelona)",
+    41.385064,
+    2.173404
+  ],
+  [
+    "Schenectady, New York (Union College)",
+    42.817572,
+    -73.930687
+  ], 
+  [
+    "Geneva, Switzerland (CERN-LHC)",
+    46.204391,
+    6.143158
+  ],
+  //[
+  //  "Cambridge, Massachusetts",
+  //  42.373611,
+  //  -71.110558
+  //]
+  [
+    "Mainz, Germany (Mainzer Mikrotron)",
     49.98419,
     8.2791
   ]
